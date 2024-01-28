d0403afbab560fad6443a9b35861a1af5b88a3af
артём иващенко <fertcool@gmail.com>
2024-01-28 15:45:00 +0300

    ветка дерева

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ from matplotlib import pyplot as plt
 from sklearn import preprocessing
 import pandas as pd
 from keras.callbacks import EarlyStopping
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor, plot_tree
 
 dfs = pd.read_excel("./i_kanalov-1.xlsx")
 dfs = dfs[["Р2О5", "К2О", "Гидролитическая кислотность", "рН водный", "рН солевой", "Гумус", "1 канал", "2 канал",
            "3 канал", "4 канал", "5 канал", "6 канал", "7 канал", "8 канал", "red color", "green color", "blue color"]]
-dfs = dfs/dfs.max()
 
-train_dfs = dfs.sample(frac=0.9, random_state=0)
-predict_dfs = dfs.drop(train_dfs.index)
+dfs = dfs/dfs.max()  # нормализация выборки
 
+train_dfs = dfs.sample(frac=0.9, random_state=0)  # тренировочный сет
+predict_dfs = dfs.drop(train_dfs.index)  # сет предсказания
+
+# выбор параметров
 train_features = np.array(train_dfs[["Р2О5", "К2О", "Гидролитическая кислотность", "рН водный", "рН солевой"]])
 train_target = np.array(train_dfs[["Гумус"]])
 predict_features = np.array(predict_dfs[["Р2О5", "К2О", "Гидролитическая кислотность", "рН водный", "рН солевой"]])
@@ -46,12 +50,15 @@ history = model.fit(np.concatenate([train_features, predict_features]), np.conca
 # loss, accuracy = model.evaluate(train_features, train_target)
 # print(f"Точность модели: {accuracy * 100:.2f}%")
 
-
+# предсказание
 train_pr = model.predict(train_features)
 predict_pr = model.predict(predict_features)
+# датафрейм для сравнения результатат на тренировочной выборке
 df_pr_train = pd.DataFrame({"prediction": train_pr.T[0], "target": train_target.T[0], "abs": abs(train_pr.T[0]-train_target.T[0])})
+# датафрейм для сравнения результатат на тестовой выборке
 df_pr_pr = pd.DataFrame({"prediction": predict_pr.T[0], "target": predict_target.T[0], "abs": abs(predict_pr.T[0]-predict_target.T[0])})
 
+# рисование графика ошибок на тренировочной и тестовой выборках(для наблюдения переобучения)
 plt.plot(history.history["loss"], label="loss")
 plt.plot(history.history["val_loss"], label="val_loss")
 plt.xlabel("Эпоха обучения")
@@ -59,6 +66,7 @@ plt.ylabel("Ошибка (MAE)")
 plt.legend()
 plt.show()
 
+# график абсолютной ошибки для всей выборки
 plt.plot(np.concatenate([df_pr_pr["abs"], df_pr_train["abs"]]), label="abs")
 plt.show()
 
@@ -73,3 +81,14 @@ plt.show()
 #
 # for i in range(count):
 #     print((predictions*max(data.humus))[i]-data.humus[i])
+
+# дерево решений
+tree = DecisionTreeRegressor(random_state=1, max_depth=5)
+tree.fit(train_features, train_target)
+pred_test = tree.predict(predict_features)
+print(pd.DataFrame({"test": pred_test, "target":predict_target.T[0]}))
+mae_test = mean_absolute_error(predict_target, pred_test)
+print(mae_test)
+plt.figure(figsize=(50, 40))
+plot_tree(tree, feature_names=["Р2О5", "К2О", "Гидролитическая кислотность", "рН водный", "рН солевой"], filled=True)
+plt.show()
